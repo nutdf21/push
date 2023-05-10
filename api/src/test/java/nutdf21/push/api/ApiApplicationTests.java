6c28bb8b8bb32d59a1b1e92be544356beaedcf51
hyeonghoon <hyeonghoon@dkitec.com>
2023-05-10 17:53:35 +0900

    setting config

--- api/src/test/java/nutdf21/push/api/ApiApplicationTests.java
+++ api/src/test/java/nutdf21/push/api/ApiApplicationTests.java
@@ -1,13 +1,24 @@
 package nutdf21.push.api;
 
+import nutdf21.push.api.jpa.AppInfoRepository;
+import nutdf21.push.api.vo.jpa.AppInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ApiApplicationTests {
 
+    @Autowired
+    AppInfoRepository appInfoRepository;
+
     @Test
     void contextLoads() {
+        System.out.println("start");
+        List<AppInfo> result = appInfoRepository.findByName("test");
+        System.out.println("end");
     }
 
 }
